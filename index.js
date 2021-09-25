0f45339d767e0b604723e97c688ca09cebcbe485
Jacob <jacobmjones17@gmail.com>
2021-09-25 18:12:52 -0400

    commit tree 1623958d1eb3f1d2ba752eae0b895017051a98a8

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function receivesAFunction(callback) {
     return callback()
 }
+
+function returnsANamedFunction() {
+    function namedFunction() {
+        console.log("named function")
+    }
+    return namedFunction
+}
+
+function returnsAnAnonymousFunction() {
+    return () => console.log('')
+}
